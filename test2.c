2e33a9bd575c70fef052f5d46bb016d348e334d7
kpradeepkumar226 <32187424+kpradeepkumar226@users.noreply.github.com>
2018-02-26 21:41:41 +0530

    Create test2.c

---
+++ test2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    float num;
+        printf("Enter a number: ");
+	    scanf("%f",&num);
+	        if (num<=0)
+		    {
+		            if (num==0)
+			              printf("You entered zero.");
+				              else
+					                printf("%.2f is negative.",num);
+							    }
+							        else
+								      printf("%.2f is positive.",num);
+								          return 0;
+}
